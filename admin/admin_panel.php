2c6fda77afeac151c32163b555f6516aed1813c7
Sagar Adhikari <sagar.adhikary675@gmail.com>
2024-08-28 16:03:32 +0545

    Add Project

--- admin/admin_panel.php
+++ admin/admin_panel.php
@@ -36,12 +36,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         try {
             // Server settings
             $mail->isSMTP();                                      // Send using SMTP
-            $mail->Host       = 'smtp.gmail.com';                 // Set the SMTP server to send through
+            $mail->Host       = '';                 // Set the SMTP server to send through
             $mail->SMTPAuth   = true;                             // Enable SMTP authentication
-            $mail->Username   = 'hostel.mmanagement@gmail.com';     // SMTP username
-            $mail->Password   = 'nyqa liep ewen arou';            // SMTP password
+            $mail->Username   = '';     // SMTP username
+            $mail->Password   = '';            // SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;    // Enable TLS encryption
-            $mail->Port       = 587;                              // TCP port to connect to
+            $mail->Port       = 25;                              // TCP port to connect to
 
             // Recipients
             $mail->setFrom('hostel.mmanagement@gmail.com', 'Hostel Management');
